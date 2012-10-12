d737a87e75bb533365e4baf3b4186d320a6f10e0
Reedy <reedy@wikimedia.org>
2012-10-12 18:46:11 +0100

    Initial commit
    Change-Id: Ia433987d870f12d6054907ea76c8e3b2871f1a31

---
+++ Calendar.i18n.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Internationalisation file for Calendar extension.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'calendar-desc'            => 'Calendar extension',
+);
+
+$messages['de'] = array(
+	'calendar-desc'            => 'Ausgabe eines Kalenders',
+);
+
+?>
